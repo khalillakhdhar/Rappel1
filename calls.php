2c517fb1d89616e7a1168eedd97864808c61834d
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-20 10:26:38 +0100

    Update calls.php

--- calls.php
+++ calls.php
@@ -1,4 +1,19 @@
 <?php
 require("Fruit.php");
-
+$apple=new Fruit();
+$banana=new Fruit();
+$apple->setNom("pomme");
+$banana->setNom("banane");
+echo $apple->getNom();
+echo "<br>";
+echo $banana->getNom();
+?>
+<br><br>
+<?php
+$ch="hello world";
+if(strpos($ch,'.')!=(strlen($ch)+1))
+{
+    $ch=$ch.'.';
+}
+echo ucfirst($ch);
 ?>
